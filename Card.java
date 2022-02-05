a46a340d05848e1d2726b4d71b7b7773a095439e
tcyang-md <70073219+tcyang-md@users.noreply.github.com>
2022-02-05 17:40:17 -0500

    Add files via upload

--- Card.java
+++ Card.java
@@ -1,10 +1,16 @@
+/*
+THIS CODE WAS MY OWN WORK, IT WAS WRITTEN WITHOUT CONSULTING
+CODE WRITTEN BY OTHER STUDENTS OR COPIED FROM ONLINE RESOURCES. Chris Yang
+*/
+
+
 import java.util.ArrayList;
 import java.util.Random;
 import javax.swing.JOptionPane;
 
 public class Card {
 
-	// TODO: Define instance variables (data members) of class Card
+	// Define instance variables (data members) of class Card
 	String value;
 	String suit;
 
@@ -12,11 +18,11 @@ public class Card {
 	// NOTE: You can overload the constructor but you must
 	// have this default constructor and it must generate a valid card
 	public Card() {
-		// TODO Add your code here ... //
 		this.value = "Ace";
 		this.suit = "Spade";
 	}
-
+	
+	// constructor with arguments, overloaded
 	public Card(String num, String suit) {
 		this.value = num;
 		this.suit = suit;
@@ -26,7 +32,7 @@ public class Card {
 	// (similar to what we did in class Employee), and make it return true if the
 	// two cards have the same suit and rank
 	// Important: Use the @Override annotation
-	// TODO Add your code here ... //
+	@Override
 	public boolean equals(Object newCard) {
 		if (newCard instanceof Card) {
 			return ((this.value).equals(((Card) newCard).value) && (this.suit).equals(((Card) newCard).suit));
@@ -43,21 +49,19 @@ public class Card {
 	// Important: Two calls to the build deck should likely return 2 different
 	// shuffles.
 	public static void buildDeck(ArrayList<Card> deck) {
-		// TODO Add your code here ... //
 
-		// set suits and values
+		// sets suits and values
 		String[] suitArray = new String[] { "Hearts", "Diamonds", "Spades", "Clubs" };
-		String[] valueArray = new String[] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen",
-				"King" };
+		String[] valueArray = new String[] { "Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King" };
 
-		// make unique cards
+		// make unique cards in order Hearts A-K, Diamonds A-K, etc...
 		for (int i = 0; i < suitArray.length; i++) {
 			for (int x = 0; x < valueArray.length; x++) {
 				deck.add(new Card(valueArray[x], suitArray[i]));
 			}
 		}
 
-		// shuffle
+		// shuffle cards
 		for (int i = 0; i < deck.size(); i++) {
 			Card temp = new Card(deck.get(i).value, deck.get(i).suit);
 			int rand = (int) (Math.random() * deck.size());
@@ -71,7 +75,8 @@ public class Card {
 	// and deals 2 cards to the dealer's hand. The deck at the end of
 	// this method should have 4 less cards than when it started.
 	public static void initialDeal(ArrayList<Card> deck, ArrayList<Card> playerHand, ArrayList<Card> dealerHand) {
-		// TODO Add your code here ... //
+		
+		// Player card first, then dealer card second, twice
 		for (int i = 0; i < 2; i++) {
 			playerHand.add(deck.get(0));
 			dealerHand.add(deck.get(1));
@@ -85,7 +90,7 @@ public class Card {
 	// The deck at the end of this method should have
 	// 1 less card than when it started.
 	public static void dealOne(ArrayList<Card> deck, ArrayList<Card> hand) {
-		// TODO Add your code here ... //
+		// deal out first random card, then remove that card
 		hand.add(deck.get(0));
 		deck.remove(0);
 	}
@@ -96,18 +101,28 @@ public class Card {
 	// it will return a 20. If the hand contains a 5 of Clubs and a
 	// Queen of Spades, this method should return a 15.
 	public static int getHandValue(ArrayList<Card> hand) {
-		// TODO Add your code here ... //
 		int handValue = 0;
 		int aceCounter = 0;
-		for (int i = 0; i < hand.size(); i++) {
-			if (hand.get(i).value.equals("Jack") || hand.get(i).value.equals("Queen") || hand.get(i).value.equals("King")) {
+		// counts number of aces that are 11s
+		
+		for (int i = 0; i < hand.size(); i++) { 
+			// iterates through whole hand
+			
+			if (hand.get(i).value.equals("Jack") || hand.get(i).value.equals("Queen") || hand.get(i).value.equals("King")) { 
+				// first checks face cards
 				handValue += 10;
-			} else if (!hand.get(i).value.equals("Ace")) {
+				
+			} else if (!hand.get(i).value.equals("Ace")) { 
+				// checks the number cards
 				handValue += Integer.parseInt(hand.get(i).value);
-			} else { // for sure aces
-				if (handValue + 11 > 21) { // ace as last drawn
+				
+			} else { 
+				// for sure aces
+				if (handValue + 11 > 21) { 
+					// ace as last card drawn if over set 1, draw more
 					handValue += 1;
-				} else {
+				} else { 
+					// checks ace, not over 21, so 11, add to acecounter counting 11's
 					handValue += 11;
 					aceCounter++;
 				}
@@ -115,7 +130,7 @@ public class Card {
 			
 		}
 		
-		// draw ace early and adjust if over 21
+		// drew an ace early and adjust if over 21
 		if (handValue > 21 && aceCounter != 0) {
 			handValue -= 10*aceCounter;
 		}
@@ -124,7 +139,6 @@ public class Card {
 
 	// Method that checks whether the given hand's value exceeds 21.
 	public static boolean checkBust(ArrayList<Card> hand) {
-		// TODO Add your code here ... //
 		if(getHandValue(hand) > 21) {
 			return true;
 		} else {
@@ -137,23 +151,24 @@ public class Card {
 	// your code should continue to hit (or get a card) if the hand is less than 17
 	// otherwise stand if the hand is greater than or equal to 17.
 	public static boolean dealerTurn(ArrayList<Card> deck, ArrayList<Card> hand) {
-		// TODO Add your code here ... //
 		while (getHandValue(hand) < 17) {
 			dealOne(deck, hand);
 		}
 		return checkBust(hand);
 	}
 
-	// TODO: Fill in the code that determines who wins. The return value should be
+	// Fill in the code that determines who wins. The return value should be
 	// 1 if the player wins and 2 if the dealer wins. Winning is determined by who
 	// has
 	// the closer value to 21 without busting (going over 21).
 	public static int whoWins(ArrayList<Card> playerHand, ArrayList<Card> dealerHand) {
-		// TODO Add your code here ... //
 		if (checkBust(playerHand) == false) {
+			// checks if player busts, if busts, dealer auto wins
 			if(checkBust(dealerHand) == true) {
+				// checks if dealer busts when player doesn't
 				return 1;
 			} else {
+				// if neither bust, winner = closer hand, if tie dealer win
 				if(21-getHandValue(playerHand) < 21-getHandValue(dealerHand)) {
 					return 1;
 				} else {
@@ -167,13 +182,11 @@ public class Card {
 	// Method that describes the card (value and suit) located at index 1
 	// in the hand. This is used to show one of the cards that the dealer has.
 	public static String displayCard(ArrayList<Card> hand) {
-		// TODO Add your code here ... //
 		return hand.get(1).value + " of " + hand.get(1).suit;
 	}
 
 	// Method that describes the cards (values and suits) in the hand.
 	public static String displayHand(ArrayList<Card> hand) {
-		// TODO Add your code here ... //
 		String handToString = "";
 		
 		for (int i = 0; i < hand.size(); i++) {
@@ -186,12 +199,11 @@ public class Card {
 	// Method that clears both the player hand and dealer hand.
 	// There should be no cards in either hand after this method is called.
 	public static void clearHands(ArrayList<Card> playerHand, ArrayList<Card> dealerHand) {
-		// TODO Add your code here ... //
 		playerHand.clear();
 		dealerHand.clear();
 	}
 
-	// TODO Add any methods as necessary
+	//  Add any methods as necessary
 
 	// Do not change anything after this!
 	public static void main(String[] args) {
